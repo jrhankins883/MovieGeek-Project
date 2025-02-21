cc136d6bcb20fc87fc0e151e046d291b293ed05a
John Hankins <jrhankins883@yahoo.com>
2025-02-21 17:02:56 -0500

    Added some list elements to the JS file.

--- Soderbergh-Tracker/frontend/script.js
+++ Soderbergh-Tracker/frontend/script.js
@@ -1,7 +1,13 @@
+const movieList = document.getElementById('movieList');
+
 fetch('http://localhost:5000/movies') // Makes the call
     .then(response => response.json()) // Converts the response to JSON
     .then(data => {
-        
+        data.forEach(movie => {
+            const li = document.createElement('li');
+            li.textContent = movie.title;
+            movieList.appendChild(li);
+        })
 })
     .catch(error => {
         console.error('Error:', error)
